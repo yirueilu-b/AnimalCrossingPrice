23f1adc2b0c886c289a650fbe223290f2be8b154
盧奕叡 <yirueilu.b@gmail.com>
2020-07-16 11:20:54 +0800

    modify favicon and add csv data

---
+++ src/Component/Navbar.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import {makeStyles} from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import useScrollTrigger from '@material-ui/core/useScrollTrigger';
+import Fab from '@material-ui/core/Fab';
+import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
+import Zoom from '@material-ui/core/Zoom';
+
+
+function ScrollTop(props) {
+    const {children, window} = props;
+    // Note that you normally won't need to set the window ref as useScrollTrigger
+    // will default to window.
+    // This is only being set here because the demo is in an iframe.
+    const trigger = useScrollTrigger({
+        target: window ? window() : undefined,
+        disableHysteresis: true,
+        threshold: 100,
+    });
+
+    const handleClick = (event) => {
+        const anchor = (event.target.ownerDocument || document).querySelector('#back-to-top-anchor');
+
+        if (anchor) {
+            anchor.scrollIntoView({behavior: 'smooth', block: 'center'});
+        }
+    };
+
+    return (
+        <Zoom in={trigger}>
+            <div onClick={handleClick} role="presentation">
+                {children}
+            </div>
+        </Zoom>
+    );
+}
+
+ScrollTop.propTypes = {
+    children: PropTypes.element.isRequired,
+    /**
+     * Injected by the documentation to work in an iframe.
+     * You won't need it on your project.
+     */
+    window: PropTypes.func,
+};
+
+export default function BackToTop(props) {
+    return (
+        <React.Fragment>
+            <CssBaseline/>
+            <AppBar>
+                <Toolbar>
+                    <Typography variant="h6">Animal Crossing Price list</Typography>
+                </Toolbar>
+            </AppBar>
+            <Toolbar id="back-to-top-anchor"/>
+            <ScrollTop {...props}>
+                <Fab size="small">
+                    <KeyboardArrowUpIcon/>
+                </Fab>
+            </ScrollTop>
+        </React.Fragment>
+    );
+}
